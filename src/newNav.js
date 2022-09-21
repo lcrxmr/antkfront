797e19af1f31f7a5ecb94138e27e0cd39baf0c92
Amaury Lacroix <amaurylacroix@macbook-air-de-amaury.home>
2022-09-21 16:23:05 +0200

    web buttons buy and how to buy

--- src/newNav.js
+++ src/newNav.js
@@ -31,12 +31,28 @@ function Navbar() {
 				<a href="#tokenomics" onClick={hideNavbar} >Tokenomics</a>
 				<a href="#roadmap" onClick={hideNavbar}>Roadmap</a>
         <a href="#team" onClick={hideNavbar}>Équipe</a>
+        <MediaQuery minWidth={1000}>
+        <a href="#team" onClick={hideNavbar}>
+          <button className="howTo">HOW TO BUY</button>
+        </a>
+        <a href="#team" onClick={hideNavbar}>
+          <button className="button">BUY</button>
+        </a>
+        </MediaQuery>
+        <MediaQuery minWidth={0} maxWidth={1000}>
+        <a href="#team" onClick={hideNavbar}>
+          <button className="howToMobile">HOW TO BUY</button>
+        </a>
+        <a href="#team" onClick={hideNavbar}>
+          <button className="buttonMobile">BUY</button>
+        </a>
+        </MediaQuery>
 				<button
 					className="nav-btn nav-close-btn"
 					onClick={showNavbar}>
 					<FaTimes size={70}/>
 				</button>
-                <div id="langButton"> 
+                {/* <div id="langButton"> 
           <MediaQuery minWidth={1000}>
             <Link to="/">
               <button className="lang">FR</button>
@@ -55,7 +71,29 @@ function Navbar() {
               <button className="langMobile">EN</button>
             </Link>
           </MediaQuery>
-            </div>
+            </div> */}
+             <div className="buyLang">
+          <MediaQuery minWidth={1000}>
+          <a href="/"><h5 className="buyLangButton" >
+              fr
+            </h5></a>
+            <h5>{""} |{""}</h5>
+            <a href="/eng"><h5 className="buyLangButton" >
+              en
+            </h5></a>
+          </MediaQuery>
+        </div>
+        <div className="buyLang">
+          <MediaQuery minWidth={0} maxWidth={1000}>
+          <a href="/"><h5 className="buyLangButtonMobile" >
+              fr
+            </h5></a>
+            <h5 className="buyLangButtonMobile">{""} |{""}</h5>
+            <a href="/eng"><h5 className="buyLangButtonMobile" >
+              en
+            </h5></a>
+          </MediaQuery>
+        </div>
 			</nav>
 			<button className="nav-btn" onClick={showNavbar}>
 				<FaBars size={70} />
